ea2440d423b1b22d13fd2e4e74625cc00b6bd1d1
gui <374779789@qq.com>
2018-07-06 17:36:32 +0800

    店铺

--- src/components/OAForm/SearchTable/index.js
+++ src/components/OAForm/SearchTable/index.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
-import { Modal, Tooltip, message } from 'antd';
-import { connect } from 'dva';
+import { Modal } from 'antd';
 import SelectTable from './selectTable';
 import RadioInput from './input';
 import CheckBoxTag from './tag';
 import Staff from './Model/Staff';
-import { getBrandAuthority, getDepartmentAuthority } from '../../../utils/utils';
+import Shop from './Model/Shop';
 import './index.less';
 
 
@@ -33,107 +32,6 @@ const defaultStyle = {
   height: '400px',
 };
 
-/**
- * 定制插件
- */
-const type = {
-  shop: {
-    makeColumns: ({ department, brand }, access) => {
-      let brandFilters = [];
-      if (brand) {
-        brand.forEach((item) => {
-          if (getBrandAuthority(item.id)) {
-            brandFilters.push({ text: item.name, value: item.id });
-          }
-        });
-      }
-      brandFilters = access ? brandFilters : (
-        brand.map(item => ({ text: item.name, value: item.id }))
-      );
-      return [
-        {
-          title: '编号',
-          dataIndex: 'shop_sn',
-          sorter: true,
-          searcher: true,
-        },
-        {
-          title: '店铺',
-          dataIndex: 'name',
-          searcher: true,
-          render: val => (
-            <Tooltip title={val} placement="right">
-              {val.length > 9 ? `${val.substr(0, 9)}...` : val}
-            </Tooltip>
-          ),
-        },
-        {
-          title: '品牌',
-          align: 'center',
-          dataIndex: 'brand_id',
-          filters: access ? brandFilters : brand.map(item => ({ text: item.name, value: item.id })),
-          render: (val) => {
-            const data = brand && brand.filter(item => item.id === val)[0];
-            return data ? data.name : '';
-          },
-        }, {
-          title: '部门',
-          dataIndex: 'department_id',
-          treeFilters: {
-            title: 'full_name',
-            value: 'id',
-            parentId: 'parent_id',
-            data: department && department.map((item) => {
-              const temp = { ...item };
-              if (access) {
-                temp.disabled = !getDepartmentAuthority(item.id);
-              }
-              return temp;
-            }),
-          },
-          render: (val) => {
-            const data = department && department.filter(item => item.id === val)[0];
-            const fullName = data ? data.full_name : '';
-            return (
-              <Tooltip title={fullName} placement="right">
-                {fullName.length > 9 ? `${fullName.substr(0, 9)}...` : fullName}
-              </Tooltip>
-            );
-          },
-        },
-        {
-          title: '店员',
-          dataIndex: 'staff',
-          searcher: true,
-          render: (val) => {
-            let shopStaff = val.map(item => item.realname);
-            shopStaff = shopStaff.join(',');
-            return (
-              <Tooltip title={shopStaff} placement="right">
-                {shopStaff.length > 9 ? `${shopStaff.substr(0, 9)}...` : shopStaff}
-              </Tooltip>
-            );
-          },
-        },
-      ];
-    },
-  },
-};
-
-/**
- * 定制插件开发的数据模型
- */
-
-@connect(({ department, brand, shop, loading }) => ({
-  brand: brand.brand,
-  brandLoading: loading.models.brand,
-  department: department.department,
-  departmentLoading: loading.effects['department/fetchDepartment'],
-  shop: shop.shop,
-  shopTotal: shop.total,
-  shopLoading: loading.models.shop,
-}))
-
 export default class SearchTable extends PureComponent {
   constructor(props) {
     super(props);
@@ -146,23 +44,6 @@ export default class SearchTable extends PureComponent {
     };
   }
 
-  componentDidMount() {
-    const { module, tableProps, dispatch } = this.props;
-    if (Object.keys(type).indexOf(module) === -1 && !tableProps) {
-      message.error(`没有找到该模型${module}的搜索代码，请在组件中添加相关模型代码`);
-      return false;
-    }
-    switch (module) {
-      case 'shop':
-        dispatch({ type: 'brand/fetchBrand' });
-        dispatch({ type: 'department/fetchDepartment', payload: {} });
-        this.fetchDataSource = this.fetchShop;
-        break;
-      default:
-        this.fetchDataSource = tableProps.fetchDataSource;
-        break;
-    }
-  }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.value !== this.props.value) {
@@ -232,62 +113,10 @@ export default class SearchTable extends PureComponent {
     onChange(value);
   };
 
-  makeTableProps = () => {
-    const { module } = this.props;
-    const { tableProps } = this.props;
-    let newTableProps = {};
-    switch (module) {
-      case 'shop':
-        newTableProps = this.makeShopProps(type[module]);
-        break;
-      default:
-        newTableProps = tableProps;
-        break;
-    }
-    return newTableProps;
-  };
-
-
-  /**
-   *
-   * 店铺shop搜索
-   */
-  fetchShop = (params) => {
-    const { dispatch } = this.props;
-    dispatch({ type: 'shop/fetchShop', payload: params });
-  };
-
-  makeShopProps = (module) => {
-    const {
-      shop,
-      shopTotal,
-      shopLoading,
-      brand,
-      brandLoading,
-      department,
-      departmentLoading,
-      authority,
-    } = this.props;
-
-    const tableProps = {
-      data: shop,
-      total: shopTotal,
-      index: 'shop_sn',
-      loading: (shopLoading || brandLoading || departmentLoading),
-    };
-
-    let access = false;
-    if (authority) {
-      access = true;
-    }
-    tableProps.columns = module.makeColumns({ brand, department }, access);
-    return tableProps;
-  };
 
   makeSearchView = () => {
-    const { multiple, placeholder, disabled, showName } = this.props;
+    const { multiple, placeholder, disabled, showName, tableProps } = this.props;
     const { visible, pushValue } = this.state;
-    const tableProps = this.makeTableProps();
     const commonProps = {
       value: pushValue,
       disabled,
@@ -318,10 +147,9 @@ export default class SearchTable extends PureComponent {
   };
 
   render() {
-    const { multiple, name, showName, title } = this.props;
+    const { multiple, name, showName, title, tableProps } = this.props;
     const { visible, modelStyle: { width }, pushValue } = this.state;
     const footer = multiple ? null : { footer: null };
-    const tableProps = this.makeTableProps();
     let selectValue = [];
     if (multiple) {
       selectValue = pushValue;
@@ -342,16 +170,16 @@ export default class SearchTable extends PureComponent {
           {...footer}
         >
           {visible && (
-          <SelectTable
-            {...tableProps}
-            name={name}
-            showName={showName}
-            multiple={multiple}
-            selectValue={selectValue}
-            fetchDataSource={this.fetchDataSource}
-            setSelectedValue={this.setTableValue}
-          />
-        )}
+            <SelectTable
+              {...tableProps}
+              name={name}
+              showName={showName}
+              multiple={multiple}
+              selectValue={selectValue}
+              fetchDataSource={this.fetchDataSource}
+              setSelectedValue={this.setTableValue}
+            />
+          )}
 
         </Modal>
       </div>
@@ -363,4 +191,4 @@ SearchTable.defaultProps = {
   onChange: () => { },
 };
 SearchTable.Staff = Staff;
-
+SearchTable.Shop = Shop;
