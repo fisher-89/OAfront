d3483dfac09be6a21acf6faa63f41f1eff685093
gui <374779789@qq.com>
2018-08-28 15:58:18 +0800

    dompl:资料列表

--- src/routes/Customer/Info/list.js
+++ src/routes/Customer/Info/list.js
@@ -1,23 +1,29 @@
 import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'dva';
 import {
-  // Card,
   Button,
-
   Divider,
-
 } from 'antd';
-
+import moment from 'moment';
 import OATable from '../../../components/OATable';
-// import OAForm, { OAModal } from '../../../components/OAForm';
-// import PageHeaderLayout from '../../../layouts/PageHeaderLayout';
+import { customerStatus } from '../../../assets/customer';
 
 @connect(({ customer, loading }) => ({
   customer: customer.customer,
-  loading: loading.effects['customer/fetchCustomer'],
+  source: customer.source,
+  tags: customer.tags,
+  loading: (
+    loading.effects['customer/fetchSource'] ||
+    loading.effects['customer/fetchTags'] ||
+    loading.effects['customer/fetchCustomer']
+  ),
 }))
 export default class Validator extends PureComponent {
   state = {};
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch({ type: 'customer/fetchSource' });
+  }
 
   fetch = (params) => {
     const { dispatch } = this.props;
@@ -25,6 +31,7 @@ export default class Validator extends PureComponent {
   }
 
   makeColumns = () => {
+    const { source, tags } = this.props;
     const columns = [
       {
         align: 'center',
@@ -36,45 +43,61 @@ export default class Validator extends PureComponent {
       {
         align: 'center',
         title: '电话',
-        dataIndex: 'phone',
+        dataIndex: 'mobile',
         // width: 160,
         searcher: true,
       },
       {
         align: 'center',
         title: '客户来源',
-        dataIndex: 'source',
+        dataIndex: 'source_id',
+        filters: source.map(item => ({ text: item.name, value: item.id })),
+        render: (key) => {
+          const value = source.find(item => item.id === key) || {};
+          return value.name;
+        },
       },
       {
         align: 'center',
         title: '客户状态',
         // width: 120,
         dataIndex: 'status',
+        filters: customerStatus.map(item => ({ text: item.name, value: item.id })),
+        render: (key) => {
+          const value = source.find(item => item.id === key) || {};
+          return value.name;
+        },
       },
       {
         // width: 240,
         align: 'center',
         title: '合作品牌',
-        dataIndex: 'brand',
+        dataIndex: 'brand_id',
       },
       {
         // width: 120,
         align: 'center',
         title: '合作时间',
-        dataIndex: 'time',
+        dataIndex: 'first_cooperation_at',
+        render: time => moment(time).format('YYYY-MM-DD'),
       },
       {
         // width: 160,
         title: '维护人',
         align: 'center',
-        dataIndex: 'staff',
+        dataIndex: 'vindicator_name',
         searcher: true,
       },
       {
         // width: 320,
         title: '标签',
         align: 'center',
-        dataIndex: 'tag',
+        dataIndex: 'has_tags',
+        render: (key) => {
+          const tagId = key.map(item => item.tag_id);
+          const value = tags.filter(tag => tagId.indexOf(tag.id)).map(item => item.name);
+          return value.join(',');
+        },
       },
       {
         title: '操作',
@@ -116,11 +139,11 @@ export default class Validator extends PureComponent {
       <OATable
         serverSide
         loading={loading}
+        data={customer.data}
+        total={customer.total}
         columns={this.makeColumns()}
         fetchDataSource={this.fetch}
         extraOperator={extraOperator}
-        data={[{ id: '1' }]}
-        total={customer && customer.total}
       />
     );
   }
