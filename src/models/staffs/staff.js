3b8531fd5ef667eaef61b51ce83d32a864e3dd62
28youth <daviezhang@vip.qq.com>
2018-09-12 10:00:31 +0800

    chore： 还原staff model

--- src/models/staffs/staff.js
+++ src/models/staffs/staff.js
@@ -3,28 +3,49 @@ import { fetchStaff, fetchStaffInfo, editStaff, deleteStaff } from '../../servic
 const store = 'staff';
 
 export default {
-  * fetchStaff({ payload }, { call, put }) {
+  * fetchStaff({ payload }, { call, put, select }) {
     try {
       const staffSn = payload.staff_sn;
-      const params = { ...payload };
-      const response = yield call(fetchStaff, params);
-      if (staffSn) {
-        yield put({
-          type: 'save',
-          payload: {
-            store,
-            staffSn,
-            data: response,
-          },
-        });
-      } else {
-        yield put({
-          type: 'save',
-          payload: {
-            store,
-            data: response,
-          },
-        });
+      const params = {
+        ...payload,
+      };
+      delete params.staff_sn;
+      const oldResponse = yield select(state => state.staffs.tableResult[JSON.stringify(params)]);
+      if (staffSn !== undefined || oldResponse === undefined) {
+        let response = [];
+        response = yield call(fetchStaff, params);
+        if (staffSn) {
+          yield put({
+            type: 'save',
+            payload: {
+              store,
+              staffSn,
+              data: response,
+            },
+          });
+        } else {
+          yield put({
+            type: 'addTableResult',
+            payload: {
+              params,
+              data: response.data,
+            },
+          });
+          yield put({
+            type: 'setTalbleTotal',
+            payload: {
+              params,
+              total: response.total,
+            },
+          });
+          yield put({
+            type: 'combine',
+            payload: {
+              store,
+              data: response.data,
+            },
+          });
+        }
       }
     } catch (err) { return err; }
   },
