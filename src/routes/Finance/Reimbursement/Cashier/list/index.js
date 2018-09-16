0ed7d94f899333ee3b649c2c97e05d47b8bc4544
Fisher <544473461@qq.com>
2018-09-16 10:36:17 +0800

    fix:报销转账页面点击查看详情抛出异常

--- src/routes/Finance/Reimbursement/Cashier/list/index.js
+++ src/routes/Finance/Reimbursement/Cashier/list/index.js
@@ -19,6 +19,7 @@ export default class extends PureComponent {
   componentWillMount() {
     const { dispatch } = this.props;
     dispatch({ type: 'reimbursement/fetchFundsAttribution' });
+    dispatch({ type: 'reimbursement/fetchStatus' });
     dispatch({ type: 'reimbursement/fetchExpenseTypes' });
   }
 
