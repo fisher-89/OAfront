0fa368840d33b6843aa9e4c1e7de646df5c0a888
gui <374779789@qq.com>
2018-09-28 10:41:21 +0800

    dompl:  颜色转换函数

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -617,3 +617,69 @@ export function analysisData(dataSource, key, keyIndex, name, dataSourceIndex) {
     .map(item => (keyIndex && item[keyIndex] !== undefined ? item[keyIndex] : item));
   return getDataSourceIndex(dataSource, keysValue, name, dataSourceIndex);
 }
+
+
+/**
+ * 十六进制->rgb
+ * @param {转换的字符串} str
+ */
+export function colorToRgb(str) {
+  let sColor = str.toLowerCase();
+  // 十六进制颜色值的正则表达式
+  const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+  // 如果是16进制颜色
+  if (sColor && reg.test(sColor)) {
+    if (sColor.length === 4) {
+      let sColorNew = '#';
+      for (let i = 1; i < 4; i += 1) {
+        sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+      }
+      sColor = sColorNew;
+    }
+    // 处理六位的颜色值
+    const sColorChange = [];
+    for (let i = 1; i < 7; i += 2) {
+      sColorChange.push(parseInt(`0x${sColor.slice(i, i + 2)}`, 16));
+    }
+    return `RGB(${sColorChange.join(',')})`;
+  }
+  return sColor;
+}
+
+/**
+ *   rgb->十六进制
+ * @param {转换的字符串} str
+ */
+export function colorToHex(str) {
+  // 十六进制颜色值的正则表达式
+  const that = str;
+  const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+  // 如果是rgb颜色表示
+  if (/^(rgb|RGB)/.test(that)) {
+    const aColor = that.replace(/(?:\(|\)|rgb|RGB)*/g, '').split(',');
+    let strHex = '#';
+    for (let i = 0; i < aColor.length; i += 1) {
+      let hex = Number(aColor[i]).toString(16);
+      if (hex.length < 2) {
+        hex = `0${hex}`;
+      }
+      strHex += hex;
+    }
+    if (strHex.length !== 7) {
+      strHex = that;
+    }
+    return strHex;
+  } else if (reg.test(that)) {
+    const aNum = that.replace(/#/, '').split('');
+    if (aNum.length === 6) {
+      return that;
+    } else if (aNum.length === 3) {
+      let numHex = '#';
+      for (let i = 0; i < aNum.length; i += 1) {
+        numHex += (aNum[i] + aNum[i]);
+      }
+      return numHex;
+    }
+  }
+  return that;
+}
