a8a8d459e2e857e0e04ec8df8de45d3e67eb08da
Fisher <544473461@qq.com>
2018-10-29 11:14:28 +0800

    fix:积分制后台获取事件记录详情接口地址错误

--- src/services/point.js
+++ src/services/point.js
@@ -76,7 +76,7 @@ export async function sorterType(params) {
 
 /* 事件日志 */
 export async function fetchEventLog(params, id) {
-  return request(`/api/pms/event-logs/${id}`, {
+  return request(`/api/pms/event-logs/${id ? `groups${id}` : ''}`, {
     method: 'GET',
     body: params,
   });
