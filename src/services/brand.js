95a151912e819e7e24746bf84f4aee9071008e63
张卫 <daviezhang@vip.qq.com>
2018-11-26 18:50:55 +0800

    chore: 升级路由

--- src/services/brand.js
+++ src/services/brand.js
@@ -1,28 +1,28 @@
 import request from '../utils/request';
 
 export async function fetchBrand(params) {
-  return request('/api/brand', {
+  return request('/api/hr/brands', {
     method: 'GET',
     body: params,
   });
 }
 
 export async function addBrand(params) {
-  return request('/api/brand', {
+  return request('/api/hr/brands', {
     method: 'POST',
     body: params,
   });
 }
 
 export async function editBrand(params, id) {
-  return request(`/api/brand/${id}`, {
+  return request(`/api/hr/brands/${id}`, {
     method: 'PATCH',
     body: params,
   });
 }
 
 export async function deleteBrand(id) {
-  return request(`/api/brand/${id}`, {
+  return request(`/api/hr/brands/${id}`, {
     method: 'DELETE',
   });
 }
--- src/services/expense.js
+++ src/services/expense.js
@@ -1,17 +1,17 @@
 import request from '../utils/request';
 
 export async function fetchExpense() {
-  return request('/api/hr/cost_brand', { method: 'GET' });
+  return request('/api/hr/cost_brands', { method: 'GET' });
 }
 
 export async function addExpense(params) {
-  return request('/api/hr/cost_brand', { method: 'POST', body: params });
+  return request('/api/hr/cost_brands', { method: 'POST', body: params });
 }
 
 export async function editExpense(params, id) {
-  return request(`/api/hr/cost_brand/${id}`, { method: 'PATCH', body: params });
+  return request(`/api/hr/cost_brands/${id}`, { method: 'PATCH', body: params });
 }
 
 export async function deleteExpense(id) {
-  return request(`/api/hr/cost_brand/${id}`, { method: 'DELETE' });
+  return request(`/api/hr/cost_brands/${id}`, { method: 'DELETE' });
 }
