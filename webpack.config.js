8faa503b34695dd2f0c698f1fbfb961c0e5c070a
Fisher <544473461@qq.com>
2018-12-01 17:48:13 +0800

    feat:webpack配置常用目录，可以使用简便的绝对路径

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+export default (webpackConfig) => {
+  return {
+    ...webpackConfig,
+    resolve: {
+      alias: {
+        src: `${__dirname}/src`,
+        components: 'src/components',
+        utils: 'src/utils',
+        routes: 'src/routes',
+      },
+    },
+  };
+};
