d668fafb24ed135e12904b0060849e00e3459b26
詹浩东 <1454443568@qq.com>
2019-01-29 13:14:01 +0800

    补充推送

---
+++ src/routes/Violation/Log/logpush.js
@@ -0,0 +1,159 @@
+import React, { PureComponent } from 'react';
+import OATable from '../../../components/OATable';
+import {
+  getFiltersData,
+  findRenderKey,
+} from '../../../utils/utils';
+
+export default class extends PureComponent {
+  state = {
+    selectedRows: [],
+    selectedRowKeys: [],
+  }
+
+  onSelectChange = (selectedRowKeys, selectedRows) => {
+    this.setState({ selectedRows, selectedRowKeys });
+  }
+
+  makeColumns = () => {
+    const { department, brand } = this.props;
+    const columns = [
+      {
+        title: '编号',
+        dataIndex: 'id',
+        width: 50,
+        fixed: 'left',
+        sorter: (a, b) => a.id - b.id,
+        defaultSortOrder: 'ascend',
+      },
+      {
+        title: '姓名',
+        dataIndex: 'staff_name',
+        searcher: true,
+        width: 60,
+        fixed: 'left',
+      },
+      {
+        title: '品牌',
+        dataIndex: 'brand_id',
+        width: 120,
+        filters: getFiltersData(brand),
+        render: key => OATable.renderEllipsis(findRenderKey(brand, key).name, true),
+      },
+      {
+        title: '部门',
+        dataIndex: 'department_id',
+        treeFilters: {
+          value: 'id',
+          title: 'name',
+          data: department,
+          parentId: 'parent_id',
+        },
+        width: 285,
+        render: key => OATable.renderEllipsis(findRenderKey(department, key).full_name, true),
+      },
+      {
+        title: '违纪日期',
+        dateFilters: true,
+        dataIndex: 'violate_at',
+        width: 100,
+      },
+      {
+        title: '大爱原因',
+        dataIndex: 'rules.name',
+        width: 120,
+        exportRender: (record) => {
+          const { name } = record.rules;
+          return name;
+        },
+        render: key => OATable.renderEllipsis(key, true),
+      },
+      {
+        title: '大爱类型',
+        dataIndex: 'rules.rule_types.name',
+        width: 105,
+        exportRender: (record) => {
+          const { name } = record.rules.rule_types;
+          return name;
+        },
+      },
+      {
+        title: '当月次数',
+        dataIndex: 'quantity',
+        width: 50,
+      },
+      {
+        title: '大爱金额',
+        dataIndex: 'money',
+        width: 50,
+      },
+      {
+        title: '分值',
+        dataIndex: 'score',
+        width: 50,
+      },
+      {
+        title: '支付状态',
+        dataIndex: 'has_paid',
+        filters: [
+          { text: '已支付', value: 1 },
+          { text: '未支付', value: 0 },
+        ],
+        width: 50,
+        render: key => (key ? '已支付' : '未支付'),
+      },
+      {
+        title: '开单日期',
+        dateFilters: true,
+        dataIndex: 'billing_at',
+        width: 100,
+      },
+      {
+        title: '开单人',
+        searcher: true,
+        dataIndex: 'billing_name',
+        width: 60,
+      },
+      {
+        title: '备注',
+        dataIndex: 'remark',
+        width: 300,
+      },
+    ];
+    return columns;
+  }
+  render() {
+    const { loading, fetchFineLog, finelog } = this.props;
+    const { selectedRowKeys, selectedRows } = this.state;
+    const rowSelection = {
+      selectedRows,
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
+    const multiOperator = [
+      {
+        text: '推送',
+        action: () => {
+        },
+      },
+      {
+        text: '清空选择',
+        action: () => {
+          this.onSelectChange([], []);
+        },
+      },
+    ];
+    return (
+      <OATable
+        loading={loading}
+        columns={this.makeColumns()}
+        fetchDataSource={fetchFineLog}
+        data={finelog.data}
+        serverSide
+        total={finelog.total}
+        rowSelection={rowSelection}
+        multiOperator={multiOperator}
+      />
+    );
+  }
+}
