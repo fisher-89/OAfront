b82e5181d178e55e988de1dc5fe5096c7bcaf453
张卫 <daviezhang@vip.qq.com>
2019-03-03 17:51:35 +0800

    ref: 改员工入转调离操作为tab窗口

--- src/routes/Hr/Staff/editProcess.js
+++ src/routes/Hr/Staff/editProcess.js
@@ -1,103 +1,79 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import {
-  Input,
-  Select,
-
-} from 'antd';
-
-import NextForm from './nextForm';
-import OAForm, { OAModal } from '../../../components/OAForm';
+import { Input } from 'antd';
+import OAForm, { OAModal, DatePicker } from '../../../components/OAForm';
 
 const FormItem = OAForm.Item;
-const { Option } = Select;
-
-
 const formItemLayout = {
   labelCol: { span: 6 },
-  wrapperCol: { span: 8 },
+  wrapperCol: { span: 14 },
 };
-
-
 @OAForm.create()
 @connect(({ loading }) => ({ loading: loading.staffs }))
 export default class extends PureComponent {
-  state = {
-    visible: false,
-  }
-
   handleSubmit = (params, onError) => {
     const { dispatch, onCancel } = this.props;
     dispatch({
       type: 'staffs/process',
       payload: params,
-      onError: (errors) => {
-        this.setState({ visible: false }, onError(errors));
-      },
-      onSuccess: () => {
-        this.setState({ visible: false }, onCancel());
-      },
+      onError: errors => onError(errors),
+      onSuccess: () => onCancel(),
     });
-  }
-
-  handleNextForm = () => {
-    this.setState({ visible: true });
   }
 
   render() {
     const {
-      form,
       loading,
       visible,
       onCancel,
       editStaff,
       validateFields,
+      validatorRequired,
       form: { getFieldDecorator },
     } = this.props;
 
-
     return (
       <React.Fragment>
-        <NextForm
-          form={form}
-          visible={this.state.visible}
-          onSubmit={validateFields(this.handleSubmit)}
-          onCancel={() => { this.setState({ visible: false }); }}
-        />
         <OAModal
           width={600}
           title="转正"
-          okText="下一步"
           loading={loading}
           visible={visible}
           style={{ top: 30 }}
           onCancel={onCancel}
-          onSubmit={validateFields(this.handleNextForm)}
+          onSubmit={validateFields(this.handleSubmit)}
         >
           {getFieldDecorator('staff_sn', {
             initialValue: editStaff.staff_sn || '',
-          })(
-            <Input type="hidden" />
-          )}
+          })(<Input type="hidden" />)}
           {getFieldDecorator('operation_type', {
             initialValue: 'employ',
-          })(
-            <Input type="hidden" />
-          )}
-          <FormItem label="姓名" {...formItemLayout}>
-            {getFieldDecorator('realname', {
-              initialValue: editStaff.realname,
+          })(<Input type="hidden" />)}
+          {getFieldDecorator('status_id', {
+            initialValue: 2,
+          })(<Input type="hidden" />)}
+          <FormItem label="员工姓名" {...formItemLayout}>
+            <span className="ant-form-text">{editStaff.realname}</span>
+          </FormItem>
+          <FormItem label="执行日期" {...formItemLayout} required>
+            {getFieldDecorator('operate_at', {
+              initialValue: '',
+              rules: [validatorRequired],
             })(
-              <Input placeholder="请输入" name="realname" disabled />
+              <DatePicker />
             )}
           </FormItem>
-          <FormItem label="状态" required {...formItemLayout}>
-            {getFieldDecorator('status_id', {
-              initialValue: 2,
+          <FormItem label="操作说明" {...formItemLayout} >
+            {getFieldDecorator('operation_remark', {
+              initialValue: '',
             })(
-              <Select name="status_id" placeholer="请选择">
-                <Option value={2}>在职</Option>
-              </Select>
+              <Input.TextArea
+                placeholder="最大长度100个字符"
+                autosize={{
+                  minRows: 4,
+                  maxRows: 6,
+                }}
+              />
             )}
           </FormItem>
         </OAModal>
